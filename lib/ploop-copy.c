c9de822733828ee3b283859becbfb82934cd46fa
Sergei A Mamonov <mrqwer88@gmail.com>
2016-04-19 15:38:17 +0300

    ploop-copy: Ploop image coruption  #PSBM-45837
    syncfs and FIFREEZE logic was not called due to typo
    
    Signed-off-by: Igor Sukhih <igor@parallels.com>

--- lib/ploop-copy.c
+++ lib/ploop-copy.c
@@ -625,7 +625,7 @@ int ploop_copy_init(struct ploop_disk_images_data *di,
 	err = ploop_get_mnt_by_dev(device, mnt, sizeof(mnt));
 	if (err == -1)
 		goto err;
-	else if (ret == 0) {
+	else if (err == 0) {
 		_h->mntfd = open(mnt, O_RDONLY);
 		if (_h->mntfd < 0) {
 			ploop_err(errno, "Can't open %s", mnt);
